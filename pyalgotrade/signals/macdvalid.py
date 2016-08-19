622ccac1c7900804d8cf23fddf9bac3b9e3d239d
runrunliuliu <liuliu.tju@gmail.com>
2016-08-19 15:15:13 +0800

    add macdvalid

---
+++ pyalgotrade/signals/macdvalid.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+# macd有效信号判定，结合macd和sar指标
+class MACDvalid(object):
+
+    def __init__(self):
+        self.__test = 0 
+
+    # 转折点的Hist值不能远小于上一波升浪的开始值
+    def HistRaTio(self, dateTime, nhist, inchist):
+        ret = 1 
+        ratio = nhist[-1] / inchist[-1][0]
+        if abs(ratio) < 0.8:
+            ret = 0
+        return ret
