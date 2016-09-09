c05d6bb3513c9331f7c7e806cad5507d662c8fd7
runrunliuliu <liuliu.tju@gmail.com>
2016-09-09 18:25:37 +0800

    fix bug

--- pyalgotrade/signals/dtvalid.py
+++ pyalgotrade/signals/dtvalid.py
@@ -42,6 +42,10 @@ class DTvalid(object):
             for i in range(0, len(arrvols)):
                 if fb[i] == 0:
                     npvols.append(arrvols[i])
+
+            if len(npvols) == 0:
+                return (None, None, None)
+
             npvols = np.array(npvols)
 
             npmax  = np.max(npvols) 
@@ -56,6 +60,10 @@ class DTvalid(object):
         (rincmax, rincmed, incmean) = stats(incvols, nvol, incfb)
         (rdesmax, rdesmed, desmean) = stats(desvols, nvol, desfb)
 
+        # 一字跌停或涨停
+        if rincmax is None or rdesmax is None:
+            return flag
+
         rmax = min([rincmax, rdesmax])
         
         if len(incvols) > len(desvols):
@@ -63,6 +71,8 @@ class DTvalid(object):
             for i in range(1, len(desvols) + 1):
                 if incfb[i * -1] == 0:
                     sums.append(incvols[i * -1])
+            if len(sums) == 0:
+                return flag
             incmean = np.mean(sums)
             # incmean = np.mean(incvols[-1 * len(desvols):])
 
