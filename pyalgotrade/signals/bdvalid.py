315f75522d5010664842ae6a98c63ab15d0ecdaa
runrunliuliu <liuliu.tju@gmail.com>
2016-10-01 18:24:04 +0800

    gold segment

--- pyalgotrade/signals/bdvalid.py
+++ pyalgotrade/signals/bdvalid.py
@@ -4,12 +4,13 @@ import numpy as np
 # 波段的有效性指标
 class BDvalid(object):
 
-    def __init__(self, direct, qshist, change, dtzq):
+    def __init__(self, direct, qshist, change, dtzq, value):
 
         self.__direct = direct 
         self.__qshist = qshist
         self.__change = change
         self.__dtzq   = dtzq
+        self.__nbar   = value
 
         self.__status = -1 
         # 上升
@@ -25,14 +26,29 @@ class BDvalid(object):
         if self.__direct == 1 and qshist == 1:
             self.__status = 21
 
-    def goldSegment(self, peek, valley, direct):
+    def goldSegment(self, peek, valley):
         # 反弹
-        ret = []
-        g = [0.809, 0.618, 0.5, 0.382, 0.236]
-        if direct == 1:
-            diff = peek - valley
+        ret  = []
+        g    = [0.764, 0.618, 0.5, 0.382, 0.236]
+        diff = peek - valley
+        baseval = -1 
+        flag    = 0
+        if self.__status == 20:
+            baseval = self.__nbar.getHigh()
+            flag    = 1 
+        if self.__status == 21:
+            baseval = self.__nbar.getClose()
+            flag    = -1 
+        if baseval > 0: 
             for i in g:
-                ret.append(valley + diff * i) 
+                fb = peek - diff * i
+                rt = (baseval - fb) / fb
+                if rt > 0 and rt < 0.01: 
+                    ret.append(flag) 
+                else:
+                    ret.append(0)
+        else:
+            ret = [0]
         return ret
 
     def period(self, dateTime, fpeek, fvalley):
@@ -45,20 +61,12 @@ class BDvalid(object):
         ret = None
         if len(fpeek) < 1 or len(fvalley) < 1:
             return ret 
-
-        # self.period(dateTime, fpeek, fvalley)
-
         ptime = fpeek[0][0]
         vtime = fvalley[0][0]
 
         peek   = datehigh[ptime] 
         valley = datelow[vtime] 
 
-        # if self.__status == 10:
-        #    print dateTime, datehigh[nowgd], valley
-
-        # gold = self.goldSegment(peek, valley, 1)
-        timediff = self.__dtzq[vtime] - self.__dtzq[ptime]
-        # print dateTime, vtime, ptime, timediff 
-
+        gold = self.goldSegment(peek, valley)
+        return (sum(gold),)
 #
