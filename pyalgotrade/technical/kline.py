87e15b057eb01e52f73f611f6d93ad7efab1dee0
runrunliuliu <liuliu.tju@gmail.com>
2016-10-14 11:00:33 +0800

    fix parameters of cross_star

--- pyalgotrade/technical/kline.py
+++ pyalgotrade/technical/kline.py
@@ -129,8 +129,6 @@ class KLineEventWindow(technical.EventWindow):
         if hi0 == lw0:
             return ret
 
-        # if vol0 / vol1  < 1.10 and diff / fenm < 0.005 and diff / (hi0 - lw0) < 0.1:
-        #    ret = 1
         if vol0 / vol1  < 1.10 and diff / fenm < 0.005 and diff / (hi0 - lw0) < 0.1:
             ret = 1
         return ret
