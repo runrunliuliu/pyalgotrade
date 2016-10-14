1982ae95c8b8d2f2006c400f11cdb7f6d4b60073
runrunliuliu <liuliu.tju@gmail.com>
2016-10-14 22:10:28 +0800

    add array of gold price

--- pyalgotrade/signals/bdvalid.py
+++ pyalgotrade/signals/bdvalid.py
@@ -30,8 +30,9 @@ class BDvalid(object):
         ret   = []
         g     = [1.0, 0.764, 0.618, 0.5, 0.382]
         fbp   = 1024
-        press = [] 
-        diff  = peek - valley
+        gprice = []
+        press  = [] 
+        diff   = peek - valley
         # 回踩黄金分割位
         if self.__status == 20 or self.__status == 21:
             nlow = self.__nbar.getLow()
@@ -49,9 +50,10 @@ class BDvalid(object):
                     ret.append(0)
                 if nhi > fb and ncls < fb:
                     press.append(cnt)
+                gprice.append(fb)
         else:
             ret = [0]
-        return (ret, fbp, press)
+        return (ret, fbp, press, gprice)
 
     def shortGoldSegment(self, dateTime, peek, valley):
         ret  = []
@@ -86,7 +88,7 @@ class BDvalid(object):
         peek   = datehigh[ptime] 
         valley = datelow[vtime] 
        
-        longGold  = ([0], 1024, [])
+        longGold  = ([0], 1024, [], [])
         shortGold = ([0], -1024)
         timediff = self.__dtzq[vtime] - self.__dtzq[ptime]
 
@@ -100,5 +102,5 @@ class BDvalid(object):
             shortGold = self.shortGoldSegment(dateTime, peek, datelow[nowgd])
 
         return (sum(longGold[0]), sum(shortGold[0]), shortGold[1], \
-                longGold[1], longGold[2])
+                longGold[1], longGold[2], longGold[3])
 #
