70bbb9e2835dacc81615a8f14517b6e8bf5f8873
runrunliuliu <liuliu.tju@gmail.com>
2017-01-17 17:11:15 +0800

    add wave5 merge

--- pyalgotrade/signals/xingtai.py
+++ pyalgotrade/signals/xingtai.py
@@ -1,42 +1,13 @@
 # coding:utf-8
 import json
+import numpy as np
+from pyalgotrade.utils import collections
+from pyalgotrade.utils import qsLineFit 
 
 
 class XINGTAI(object):
 
-    def __init__(self, dateTime, tups):
-
-        self.__nowdt = dateTime
-
-        # 周期
-        self.__dtzq  = tups[0] 
-
-        # 峰点
-        self.__fpeek   = tups[1]
-        # 谷点
-        self.__fvalley = tups[2]
-
-        # open
-        self.__open  = tups[3]
-
-        # high
-        self.__high  = tups[4]
-
-        # low
-        self.__low   = tups[5]
-
-        # close
-        self.__close = tups[6]
-
-        # nowgd 
-        self.__nowgd = tups[7]
-
-        # qshist
-        self.__qshist = tups[8]
-
-        # direct
-        self.__direct = tups[9]
-
+    def __init__(self):
         # final output
         self.__nqs    = 0
         self.__preqs  = 0
@@ -44,6 +15,44 @@ class XINGTAI(object):
         self.__gs     = []
         self.__zc     = {} 
         self.__jd     = {} 
+
+        self.__incsector = []
+        self.__dessector = []
+       
+        # QUSHI HISTORY
+        self.__history_qs = collections.ListDeque(10)
+
+    def initTup(self, dateTime, tups):
+        self.__nowdt = dateTime
+        # 周期
+        self.__dtzq  = tups[0] 
+        # 峰点
+        self.__fpeek   = tups[1]
+        # 谷点
+        self.__fvalley = tups[2]
+
+        # open
+        self.__open  = tups[3]
+        # high
+        self.__high  = tups[4]
+        # low
+        self.__low   = tups[5]
+        # close
+        self.__close = tups[6]
+
+        # nowgd 
+        self.__nowgd  = tups[7]
+        # qshist
+        self.__qshist = tups[8]
+        # direct
+        self.__direct = tups[9]
+        self.__format = '%Y-%m-%d'
+        if tups[10] == '15mink':
+            self.__format = '%Y-%m-%d-%H'
+
+        self.__update = 0
+        if tups[11] == 1 or tups[12] == -1:
+            self.__update = 1
 
     # Main Module
     def run(self):
@@ -75,6 +84,9 @@ class XINGTAI(object):
         qs['zc']  = self.__zc
         qs['jd']  = self.__jd
 
+        qs['isector']  = self.__incsector
+        qs['dsector']  = self.__dessector
+
         out['qs'] = qs
 
         return out
@@ -98,9 +110,10 @@ class XINGTAI(object):
 
         tup = (peek0, peek1, vall0, vall1)
         bqs = self.basicQS(tup)
-
+        if self.__update == 1:
+            self.__history_qs.append(bqs)
         pqs = bqs[0] 
-        nqs = pqs  
+        nqs = pqs
 
         if pqs == 1101 or pqs == 1201 or pqs == 1202:
             if ngdhigh >= bqs[1]:
@@ -137,6 +150,11 @@ class XINGTAI(object):
 
         nowclose = self.__close[self.__nowdt]
         self.zcjd(pqs, nowclose, tup)
+
+        self.__incsector = self.incSectorLine(self.__nowdt)
+        self.__dessector = self.desSectorLine(self.__nowdt)
+
+        self.szline(self.__nowdt)
    
     # 反转
     def fanzhuan(self):
@@ -146,10 +164,104 @@ class XINGTAI(object):
     def chixu(self):
         pass
 
+    # 合并QS, 减少毛刺
+    def mergeQS(self, dateTime, qslist):
+        fgds  = set()
+        QS    = []
+        tnum  = 3 
+        count = 1
+        for i in range(1, len(qslist) + 1):
+            if count > tnum:
+                break
+            struct = qslist[-1 * i][5]
+            # 背离产生的异常结构
+            zqarr  = np.asarray(qslist[-1 * i][7])
+            if min(zqarr) < 0:
+                continue
+            # 如果相邻拐点bars过少, 前溯1个基本结构, 默认是3个基本结构
+            index = -1
+            if min(zqarr) <= 3:
+                index = np.argmin(zqarr) 
+                flag = 0
+                for k in range(0, 2):
+                    tday = struct[index + k]['d'] 
+                    if tday in fgds:
+                        flag = 1
+                        break
+                    else:
+                        fgds.add(tday)
+                if flag == 0:
+                    tnum = tnum + 2
+            QS.append(qslist[-1 * i] + (index,))
+            count = count + 1
+        return QS
+
+    # 消除毛刺的5浪重构
+    def wave5construct(self, dateTime, QS):
+        used = set()
+        w5 = () 
+        i  = 1
+        def add(w5, used, new):
+            for item in new:
+                if item not in used:
+                    w5 = w5 + (item,)
+                used.add(item)
+            return (w5, used)
+
+        while(i < len(QS) + 1):
+            q = QS[-1 * i]
+            # 基本结构无minbar
+            if q[-1] == -1:
+                if i == 1:
+                    # w5 = w5 + q[8][0:3]
+                    (w5, used) = add(w5, used, q[8][0:3])
+                elif i == len(QS):
+                    # w5 = w5 + q[8][2:4]
+                    (w5, used) = add(w5, used, q[8][2:4])
+                else:
+                    # w5 = w5 + (q[8][2],)
+                    (w5, used) = add(w5, used, (q[8][2],))
+                step = 1 
+            # 基本结构minbar居中
+            if q[-1] == 1:
+                if q[0] == 1302 or q[0] == 1301 \
+                        or q[0] == 2102 or q[0] == 2103:
+                    # w5 = w5 + (q[8][3],)
+                    (w5, used) = add(w5, used, (q[8][3],))
+                    step = 2
+                else:
+                    # w5 = w5 + (q[8][1],)
+                    (w5, used) = add(w5, used, (q[8][1],))
+                    step = 1
+            # 基本结构minbar居始
+            if q[-1] == 0:
+                # w5 = w5 + q[8][2:4]
+                (w5, used) = add(w5, used, q[8][2:4])
+                step = 2
+            # 基本结构minbar居末尾
+            if q[-1] == 2:
+                if i == len(QS):
+                    # w5 = w5 + q[8][0:3]
+                    (w5, used) = add(w5, used, q[8][0:3])
+                else:
+                    # w5 = w5 + q[8][0:2]
+                    (w5, used) = add(w5, used, q[8][0:2])
+                step = 1
+            if i == len(QS) - 1:
+                step = 1
+            i = i + step
+        print w5
+
+    # 速阻线
+    def szline(self, dateTime):
+        qslist = self.__history_qs
+        QS = self.mergeQS(dateTime, qslist)
+        self.wave5construct(dateTime, QS)
+
     # 支撑线和阻挡线
     def zcjd(self, nqs, nowclose, tup):
-        peek1 = tup[1]; speek1 = peek1.strftime('%Y-%m-%d')
-        vall1 = tup[3]; svall1 = vall1.strftime('%Y-%m-%d')
+        peek1 = tup[1]; speek1 = peek1.strftime(self.__format)
+        vall1 = tup[3]; svall1 = vall1.strftime(self.__format)
 
         high1 = self.__high[peek1]
         low1 = self.__low[vall1]
@@ -181,15 +293,64 @@ class XINGTAI(object):
         return out
 
     # 上升扇形
-    def incSectorLine(self):
+    def incSectorLine(self, dateTime):
         ret = []
         if len(self.__fvalley) < 4:
             return ret
-        print self.__fvalley[0:4]
+        tgds = self.__fvalley
+        ngd = tgds[3]
+        
+        nind1  = self.__dtzq[tgds[1][0]]
+        close1 = self.__close[tgds[1][0]]
+        nind2  = self.__dtzq[tgds[2][0]]
+        close2 = self.__close[tgds[2][0]]
+
+        if ngd[1] < tgds[0][1] and ngd[1] < tgds[1][1] and ngd[1] < tgds[2][1]:
+            for i in range(0, 3):
+                v     = (ngd[0], ngd[1], tgds[i][0], tgds[i][1])
+                qsfit = qsLineFit.QsLineFit.initFromTuples(v, self.__dtzq)
+                if i == 0:
+                    if close1 < qsfit.compute(nind1) or close2 < qsfit.compute(nind2):
+                        return [] 
+                if i == 1:
+                    if close2 < qsfit.compute(nind2):
+                        return [] 
+                ret.append(qsfit)
+        out = []
+        if len(ret) == 3:
+            for r in ret:
+                out.append(r.toDICT(self.__dtzq[dateTime]))
+        return out
 
     # 下降扇形
-    def desSectorLine(self):
-        pass
+    def desSectorLine(self, dateTime):
+        ret = []
+        if len(self.__fpeek) < 4:
+            return ret
+        tgds = self.__fpeek
+        ngd = tgds[3]
+        
+        nind1  = self.__dtzq[tgds[1][0]]
+        close1 = self.__close[tgds[1][0]]
+        nind2  = self.__dtzq[tgds[2][0]]
+        close2 = self.__close[tgds[2][0]]
+
+        if ngd[1] > tgds[0][1] and ngd[1] > tgds[1][1] and ngd[1] > tgds[2][1]:
+            for i in range(0, 3):
+                v     = (ngd[0], ngd[1], tgds[i][0], tgds[i][1])
+                qsfit = qsLineFit.QsLineFit.initFromTuples(v, self.__dtzq)
+                if i == 0:
+                    if close1 > qsfit.compute(nind1) or close2 > qsfit.compute(nind2):
+                        return [] 
+                if i == 1:
+                    if close2 > qsfit.compute(nind2):
+                        return [] 
+                ret.append(qsfit)
+        out = []
+        if len(ret) == 3:
+            for r in ret:
+                out.append(r.toDICT(self.__dtzq[dateTime]))
+        return out
 
     # 三种最基本的走势定义
     #  1 -- 上升
@@ -197,15 +358,18 @@ class XINGTAI(object):
     #  3 -- 下降
     def basicQS(self, tup):
         ret = 0 
-        peek0 = tup[0]; speek0 = peek0.strftime('%Y-%m-%d')
-        peek1 = tup[1]; speek1 = peek1.strftime('%Y-%m-%d')
-        vall0 = tup[2]; svall0 = vall0.strftime('%Y-%m-%d')
-        vall1 = tup[3]; svall1 = vall1.strftime('%Y-%m-%d')
+        peek0 = tup[0]; speek0 = peek0.strftime(self.__format)
+        peek1 = tup[1]; speek1 = peek1.strftime(self.__format)
+        vall0 = tup[2]; svall0 = vall0.strftime(self.__format)
+        vall1 = tup[3]; svall1 = vall1.strftime(self.__format)
 
         high0 = self.__high[peek0]; high1 = self.__high[peek1]
         low0 = self.__low[vall0]; low1 = self.__low[vall1]
 
+        # print self.__nowdt, tup, high0, high1, low0, low1
+        zhouqi = None
         struct = None 
+        timegd = None
         if self.__direct == -1:
             if high0 >= high1:
                 if low0 >= high1:
@@ -223,6 +387,12 @@ class XINGTAI(object):
             struct = [{"d":speek1, 'v':high1}, {"d":svall1, 'v':low1}, \
                       {"d":speek0, 'v':high0}, {"d":svall0, 'v':low0}]
 
+            zq1 = self.__dtzq[vall1] - self.__dtzq[peek1] + 1
+            zq2 = self.__dtzq[peek0] - self.__dtzq[vall1] + 1
+            zq3 = self.__dtzq[vall0] - self.__dtzq[peek0] + 1
+            zhouqi = (zq1, zq2, zq3)
+            timegd = (peek1, vall1, peek0, vall0)
+
         if self.__direct == 1:
             if low0 >= low1:
                 if high0 >= high1:
@@ -239,8 +409,13 @@ class XINGTAI(object):
                         ret = 2103
             struct = [{"d":svall1, 'v':low1}, {"d":speek1, 'v':high1}, \
                       {"d":svall0, 'v':low0}, {"d":speek0, 'v':high0}]
+            zq1 = self.__dtzq[peek1] - self.__dtzq[vall1] + 1
+            zq2 = self.__dtzq[vall0] - self.__dtzq[peek1] + 1
+            zq3 = self.__dtzq[peek0] - self.__dtzq[vall0] + 1
+            zhouqi = (zq1, zq2, zq3)
+            timegd = (vall1, peek1, vall0, peek0)
 
         gs = self.goldseg(max(high0, high1), min(low0, low1))
 
-        return (ret, high0, low0, high1, low1, struct, gs)
+        return (ret, high0, low0, high1, low1, struct, gs, zhouqi, timegd)
 #
